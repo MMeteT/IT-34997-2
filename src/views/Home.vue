9c273a905486b55ef24a651ecadb4d5339950b19
Murat Mete Türkmen <mt@Murats-MacBook-Pro.local>
2024-07-16 22:00:50 +0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<script>
+import HomeHeader from "../components/HomeHeader.vue"
+import PokemonList from "../components/PokemonList.vue"
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        HomeHeader,
+        PokemonList
+    },
+}
+</script>
+<template>
+<div class='container mx-auto w-640 my-4 p-2'>
+
+    <HomeHeader />
+
+</div>
+
+<div class="grid grid-cols-4">
+
+    <PokemonList />
+
+</div>
+
+
+
+
+</template>
